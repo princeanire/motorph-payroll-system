da1efd5aaf6a512f1d45aff02c1aa2373d705996
Prince Andrew Anire <andrewcustomerhelper@gmail.com>
2025-02-16 13:35:05 +0000

    Add Employee class with constructor and toString method

---
+++ src/main/java/motorph/Employee.java
@@ -0,0 +1,29 @@
+package motorph;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Employee {
+    public int employeeNumber;
+    public String lastName;
+    public String firstName;
+    public LocalDate birthday;
+
+    public Employee(int employeeNumber, String lastName, String firstName, LocalDate birthday,
+            String address, String phoneNumber, String sssNumber, String philhealthNumber, String tinNumber,
+            String pagibigNumber, EmploymentStatus status, String position, String immediateSupervisor,
+            BigDecimal basicSalary, BigDecimal riceSubsidy, BigDecimal phoneAllowance, BigDecimal clothingAllowance,
+            BigDecimal grossSemimonthlyRate, BigDecimal hourlyRate) {
+        this.employeeNumber = employeeNumber;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthday = birthday;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d, %s %s, %s", employeeNumber, firstName, lastName, birthday.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+    }
+}
