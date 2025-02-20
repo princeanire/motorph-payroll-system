eceee7d7f391545c01e72ad955d9ecd8c8f9eb1c
Prince Andrew Anire <andrewcustomerhelper@gmail.com>
2025-02-20 02:48:09 +0000

    Refactor App to utilize EmployeeDatabaseService for fetching attendance records

--- src/main/java/motorph/App.java
+++ src/main/java/motorph/App.java
@@ -1,21 +1,14 @@
 package motorph;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.List;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        Employee antonioLim = new Employee(10002, "Lim", "Antonio",
-                LocalDate.of(1988, 06, 19), "San Antonio De Padua 2, Block 1 Lot 8 and 2, Dasmarinas, Cavite",
-                "171-867-411", "52-2061274-9",
-                "331735646338", "683-102-776-000", "663904995411", EmploymentStatus.Regular,
-                "Chief Operating Officer", "Garcia, Manuel III", new BigDecimal(60000), new BigDecimal(1500),
-                new BigDecimal(2000), new BigDecimal(1000), new BigDecimal(30000), new BigDecimal(357.14));
-
-        System.out.println(antonioLim.toString());
-
+        EmployeeDatabaseService.getAttendanceRecords();
+        List<AttendanceRecord> antonioLimAttendanceRecords = EmployeeDatabaseService
+                .getAttendanceRecordsByEmployeeId(10002);
+        for (AttendanceRecord attendanceRecord : antonioLimAttendanceRecords) {
+            System.out.println(attendanceRecord.employeeId);
+        }
     }
 }
