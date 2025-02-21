20b6ceacd3deeb704c3169e9e074b27b263e6b4f
Prince Andrew Anire <andrewcustomerhelper@gmail.com>
2025-02-21 00:05:33 +0000

    Add methods to load and retrieve employee details in EmployeeDatabaseService

--- src/main/java/motorph/EmployeeDatabaseService.java
+++ src/main/java/motorph/EmployeeDatabaseService.java
@@ -3,12 +3,14 @@ package motorph;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class EmployeeDatabaseService {
     public static List<AttendanceRecord> attendanceRecords;
+    public static List<Employee> employees;
 
     public static List<AttendanceRecord> getAttendanceRecords() {
         if (attendanceRecords == null) {
@@ -25,7 +27,7 @@ public class EmployeeDatabaseService {
                     .build()
                     .parse();
         } catch (FileNotFoundException fileNotFoundException) {
-            System.out.println("Employee Database CSV file not found: " + fileNotFoundException);
+            System.out.println("Attendance Records Database CSV file not found: " + fileNotFoundException);
         }
     }
 
@@ -33,6 +35,30 @@ public class EmployeeDatabaseService {
         return getAttendanceRecords().stream()
                 .filter(record -> record.employeeId == employeeId)
                 .collect(Collectors.toList());
+    }
+
+    private static void loadEmployeeDetails() {
+        try {
+            employees = new CsvToBeanBuilder<Employee>(
+                    new FileReader("src/main/resources/employee-details.csv"))
+                    .withType(Employee.class)
+                    .build()
+                    .parse();
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("Employee Details Database CSV file not found: " + fileNotFoundException);
+        }
+    }
+
+    public static List<Employee> getEmployeeDetails() {
+        if (employees == null) {
+            loadEmployeeDetails();
+        }
+        return employees;
+    }
+
+    public static Optional<Employee> getEmployeeDetailsByEmployeeId(int employeeId) {
+        return getEmployeeDetails().stream()
+                .filter(employeeDetails -> employeeDetails.employeeId == employeeId).findFirst();
     }
 
 }
