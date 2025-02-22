ccdf6df3b945efc87e4789f484af1e12f564939b
Prince Andrew Anire <andrewcustomerhelper@gmail.com>
2025-02-22 06:54:15 +0000

    Rename methods in EmployeeDatabaseService for clarity: getAttendanceRecords() to getAllAttendanceRecords() and getEmployeeDetails() to getAllEmployeeDetails()

--- src/main/java/motorph/App.java
+++ src/main/java/motorph/App.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        EmployeeDatabaseService.getAttendanceRecords();
+        EmployeeDatabaseService.getAllAttendanceRecords();
         List<AttendanceRecord> antonioLimAttendanceRecords = EmployeeDatabaseService
                 .getAttendanceRecordsByEmployeeId(10002);
         for (AttendanceRecord attendanceRecord : antonioLimAttendanceRecords) {
             System.out.println(attendanceRecord.employeeId);
         }
 
-        EmployeeDatabaseService.getEmployeeDetails();
+        EmployeeDatabaseService.getAllEmployeeDetails();
         Employee antonioLim = EmployeeDatabaseService.getEmployeeDetailsByEmployeeId(10002).get();
         System.out.println("Antonio Lim's hourly rate is " + antonioLim.hourlyRate.toString());
     }
