b427e30ee4b8690d43af16bcced2a0438cd654f4
Prince Andrew Anire <andrewcustomerhelper@gmail.com>
2025-03-11 09:37:28 +0800

    Calculate weekly salary for all employees in main method

--- src/main/java/motorph/App.java
+++ src/main/java/motorph/App.java
@@ -1,18 +1,40 @@
 package motorph;
 
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        EmployeeDatabaseService.getAllAttendanceRecords();
-        List<AttendanceRecord> antonioLimAttendanceRecords = EmployeeDatabaseService
-                .getAttendanceRecordsByEmployeeId(10002);
-        for (AttendanceRecord attendanceRecord : antonioLimAttendanceRecords) {
-            System.out.println(attendanceRecord.employeeId);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10001);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10002);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10003);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10004);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10005);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10006);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10007);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10008);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10009);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10010);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10011);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10012);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10013);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10014);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10015);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10016);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10017);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10018);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10019);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10020);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10021);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10022);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10023);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10024);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10025);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10026);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10027);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10028);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10029);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10030);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10031);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10032);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10033);
+        EmployeeDatabaseService.calculateEmployeeWeeklySalary(10034);
         }
-
-        EmployeeDatabaseService.getAllEmployeeDetails();
-        Employee antonioLim = EmployeeDatabaseService.getEmployeeDetailsByEmployeeId(10002).get();
-        System.out.println("Antonio Lim's hourly rate is " + antonioLim.hourlyRate.toString());
     }
-}
